347fecff419044e0bc4862ae9356bd354f732675
aryan-rai <72652989+aryan-rai@users.noreply.github.com>
2022-05-03 12:51:18 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+@app.route("/predict-alphabet", methods=["POST"])
+def predict_data():
+ image = request.files.get("alphabet")
+ prediction = get_prediction(image)
+return jsonify({
+ "prediction": prediction
+ }), 200
